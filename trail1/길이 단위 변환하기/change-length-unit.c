64839ab4e497dbe929eb1940a4bf6785abe4e3f3
albegaa <123287370+albegaa@users.noreply.github.com>
2026-09-01 16:27:49 +0900

    [Passed] 길이 단위 변환하기 (1ms, 0MB)

---
+++ trail1/길이 단위 변환하기/change-length-unit.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    double ft = 9.2, mi = 1.3;
+    printf("%.1lfft = %.1lfcm", ft, ft*30.48);
+    printf("\n%.1lfmi = %.1lfcm", mi, mi*160934);
+    return 0;
+}
