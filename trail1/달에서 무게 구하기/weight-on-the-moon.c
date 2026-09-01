3259cadc0b205861cffbc16fd65cbadd15397046
albegaa <123287370+albegaa@users.noreply.github.com>
2026-09-01 16:22:54 +0900

    [Passed] 달에서 무게 구하기 (1ms, 0MB)

---
+++ trail1/달에서 무게 구하기/weight-on-the-moon.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int weight = 13;
+    double ratio = 0.165;
+    printf("%d * %6lf = %6lf", weight, ratio, weight*ratio);
+    return 0;
+}
